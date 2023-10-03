a22c46361eb932031ea249cfb4f23151d3d4804e
jcalixto202020774 <j.calixto@uniandes.edu.co>
2023-10-03 15:57:32 -0500

    Create Post Model

--- guarap/Model/Post.swift
+++ guarap/Model/Post.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+struct Post: Identifiable, Hashable, Codable {
+    
+    // Atributes of a post
+    
+    let id: String
+    let ownerUid: String
+    let title: String
+    let description: String
+    let ups: Int
+    let downs: Int
+    let image: String
+    let timeStamp: Date
+    let category: String
+    
+}

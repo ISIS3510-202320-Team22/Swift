6122325bbd1c40f207cd25300a065464b95ba309
jcalixto202020774 <j.calixto@uniandes.edu.co>
2023-10-03 14:14:37 -0500

    FeedCell - Added user, image and buttons - Missing how to deploy comments

--- guarap/Feed Group/FeedCell.swift
+++ guarap/Feed Group/FeedCell.swift
@@ -9,7 +9,74 @@ import SwiftUI
 
 struct FeedCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            // Image and username + publishing time
+            HStack{
+                Image("avatar")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 60, height:60)
+                    .clipShape(Circle())
+                VStack(alignment: .leading){
+                    Text("@juandicanu202")
+                        .font(.system(size: 20))
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    Text("25/10 4:06pm")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray)
+                }
+                
+                Spacer()
+            }.padding(.leading)
+            
+            // Post
+            
+            Image("bobo_sunset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 400)
+                .cornerRadius(15)
+                .clipShape(Rectangle())
+                .padding(15)
+            
+            // Action buttons
+            
+            HStack(spacing: 20){
+                // arrowtriangle.up.circle.fill
+                // hand.thumbsup.fill
+                // arrow.up.heart.fill
+                Spacer()
+                Button{
+                    print("Up post")
+                } label: {
+                    Image(systemName: "arrow.up.heart.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 35, height:35)
+                }
+                Button{
+                    print("Up post")
+                } label: {
+                    Image(systemName: "arrow.down.heart.fill")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 35, height:35)
+                }
+                
+            }.padding(.trailing)
+                .foregroundColor(.black)
+            
+            // Comments
+            HStack{
+                Text("@sadiomane ").fontWeight(.semibold) +
+                Text("Grande calixx")
+            }
+            .frame(maxWidth: .infinity, alignment:.leading)
+            .font(.footnote)
+            .padding(.leading,15)
+            .padding(.top,1)
+        }
     }
 }
 

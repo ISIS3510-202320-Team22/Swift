b4fc7b6650b7c8e6f87bd1f5f349f60a6cd31c92
Esteban Gonzalez Ruales <74991415+est-gonzalezr@users.noreply.github.com>
2023-10-10 20:40:08 -0500

    Update Account View and delete previous authservice

--- guarap/Settings Group/AccountView.swift
+++ guarap/Settings Group/AccountView.swift
@@ -1,15 +1,78 @@
 //
-//  AccountView.swift
+//  ProfileView.swift
 //  guarap
 //
-//  Created by Esteban Gonzalez Ruales on 30-09-2023.
+//  Created by Nathalia Quiroga 2/10/23.
 //
 
 import SwiftUI
 
 struct AccountView: View {
     var body: some View {
-        Text("Here you can manage\n all your account settings")
+        NavigationView {
+            VStack{
+                HStack {
+                    NavigationLink(destination: SettingsView()) {
+                        Text("Settings")
+                    }
+                    .padding()
+                    Spacer()
+                    
+                    NavigationLink(destination: PublishView()) {
+                        Text("Post")
+                    }
+                    .padding()
+                }
+                Text("Guarap")
+                    .font(.title)
+    
+                ZStack{
+                    
+                    VStack(alignment: .center){
+                        Image(systemName: "person.crop.circle")
+                            .resizable()
+                            .frame(width: 200, height: 200, alignment: .topLeading)
+                        Text("@juandicanu202")
+                            .font(.system(size: 30))
+                        Text("Favorite Categories")
+                            .font(.system(size: 25))
+                            .bold()
+                            .padding()
+                        Button("Category 1"){
+                
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 60)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                        Button("Category 2"){
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 60)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                        Button("Category 3"){
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 60)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                        Button("Category 4"){
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 200, height: 60)
+                        .background(Color.red)
+                        .cornerRadius(10)
+                    }
+                    
+                    Spacer()
+                }
+                Spacer()
+            }
+        }
     }
 }
 

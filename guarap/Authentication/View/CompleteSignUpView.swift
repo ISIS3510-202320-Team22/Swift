50d3659d3d7dd302b62566fe42950048b025ee69
Esteban Gonzalez Ruales <74991415+est-gonzalezr@users.noreply.github.com>
2023-10-10 20:30:51 -0500

    Add Authentication files of Nathalia

---
+++ guarap/Authentication/View/CompleteSignUpView.swift
@@ -0,0 +1,45 @@
+//
+//  CreateUsernameView.swift
+//  guarap
+//
+//  Created by Quiroga Alfaro Nathalia Alexandra on 10/10/23.
+//
+
+import SwiftUI
+
+struct CompleteSignUpView: View {
+    @EnvironmentObject var viewModel: RegistrationViewModel
+
+    var body: some View {
+        VStack(spacing: 12) {
+            Text ("Welcome to Guarap, \(viewModel.username)!")
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight (.bold)
+                .padding (.top)
+            Text("Click below to complete registration")
+                .font (.footnote)
+                .foregroundColor (.gray)
+                .multilineTextAlignment (.center)
+                .padding (.horizontal, 24)
+            Button{
+                Task{ try await viewModel.createUser()}
+            } label: {
+                Text ("Complete Sing Up" )
+                    .font (. subheadline)
+                    .fontWeight (.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 360,
+                           height: 44)
+                    .background(Color(.red))
+                    .cornerRadius(8)
+            }
+            .padding (.vertical)
+        }
+    }
+}
+
+struct CompleteSignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteSignUpView()
+    }
+}

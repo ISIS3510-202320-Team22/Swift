a68634b313a0828934ac8533039ac43580911a93
est-gonzalezr <74991415+est-gonzalezr@users.noreply.github.com>
2023-10-28 16:03:48 -0500

    Change schema

--- guarap/DAOs/PostDAOFirebase.swift
+++ guarap/DAOs/PostDAOFirebase.swift
@@ -83,11 +83,11 @@ class PostDAOFirebase: PostDAO {
                 do {
                     if let user = postDict["user"] as? String,
                        let description = postDict["description"] as? String,
-                       let upVotes = postDict["upVotes"] as? Int,
-                       let downVotes = postDict["downVotes"] as? Int,
+                       let upVotes = postDict["upvotes"] as? Int,
+                       let downVotes = postDict["downvotes"] as? Int,
                        let reported = postDict["reported"] as? Bool,
                        let address = postDict["address"] as? String,
-                       let dateTime = postDict["dateTime"] as? Timestamp {
+                       let dateTime = postDict["date"] as? Timestamp {
                         
                         var image = ""
                         if let unwrappedImage = postDict["image"] as? String {

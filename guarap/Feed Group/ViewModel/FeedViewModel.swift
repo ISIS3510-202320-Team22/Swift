ada5c28837f7e91c7d4663b0a70d22a01e59e39a
est-gonzalezr <74991415+est-gonzalezr@users.noreply.github.com>
2023-10-30 00:35:26 -0500

    Implement caching strategy for posts

--- guarap/Feed Group/ViewModel/FeedViewModel.swift
+++ guarap/Feed Group/ViewModel/FeedViewModel.swift
@@ -9,22 +9,65 @@ import Foundation
 import Firebase
 import SwiftUI
 
-class FeedViewModel: ObservableObject{
+class FeedViewModel: ObservableObject {
     @AppStorage("lastCategory") var lastCategory = "Generic"
     @Published var posts = [Post]()
     @Published var categoryString = "Generic"
     let guarapRepo = GuarapRepositoryImpl.shared
-    
-    init(){
+
+    // Create a cache for posts
+    private var postCache = CategoryPostCache()
+
+    init() {
         if lastCategory == "" {
             lastCategory = "Generic"
         }
         categoryString = lastCategory
-        Task{ try await fetchPosts(category: categoryString)}
+
+        // Attempt to fetch posts from cache first
+        if let cachedPosts = postCache.getPosts(forCategory: categoryString) {
+            posts = cachedPosts
+        }
+
+        // Then, update the posts by fetching from the repository
+        Task {
+            do {
+                let fetchedPosts = try await guarapRepo.getPostsByCategory(categoryName: categoryString)
+                // Update the posts property
+                posts = fetchedPosts
+                // Store the fetched posts in the cache
+                postCache.setPosts(fetchedPosts, forCategory: categoryString)
+            } catch {
+                // Handle errors
+                print("Error fetching posts: \(error)")
+            }
+        }
     }
     
     @MainActor
     func fetchPosts(category: String) async throws {
-        posts = try await guarapRepo.getPostsByCategory(categoryName: category)
+        if let cachedPosts = postCache.getPosts(forCategory: category) {
+            posts = cachedPosts
+            print("Accessing cache")
+        } else {
+            print("Accessing from web")
+            let fetchedPosts = try await guarapRepo.getPostsByCategory(categoryName: category)
+            postCache.setPosts(fetchedPosts, forCategory: category)
+            posts = fetchedPosts
+        }
+    }
+    
+    @MainActor
+    func fetchPostsFromWeb(category: String) async throws {
+        do {
+            let webPosts = try await guarapRepo.getPostsByCategory(categoryName: category)
+            postCache.setPosts(webPosts, forCategory: category)
+            posts = webPosts
+            print("Get posts directly from the web")
+        } catch {
+            // Handle errors
+            print("Error fetching posts: \(error)")
+            print("Error getting directly from web")
+        }
     }
 }

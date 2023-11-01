6871a9851afabf9f5576ad7997f1217d66a0e5cd
Nathalia Alexandra Quiroga Alfaro <85660281+Nathaliaq16@users.noreply.github.com>
2023-11-01 10:39:31 -0500

    Update SwiftUIView.swift

--- guarap/Publish Group/PublishView/SwiftUIView.swift
+++ guarap/Publish Group/PublishView/SwiftUIView.swift
@@ -9,32 +9,68 @@ import SwiftUI
 import CoreLocation 
 import MapKit
 
-
-import SwiftUI
-import MapKit
-import CoreLocation
-
-struct SwiftUIView: View {
+struct ContentView: View {
     @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
-        span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        center: CLLocationCoordinate2D(latitude: 4.6097, longitude: -74.0817),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
     )
+    
     @StateObject var locationManager = LocationManager()
-
+    @State private var address: String?
+    
     var body: some View {
-        Map(coordinateRegion: $region, showsUserLocation: true)
-            .onAppear(perform: {
-                locationManager.requestLocation()
-                if let location = locationManager.lastKnownLocation {
-                    region.center = location.coordinate
+        VStack {
+            Map(coordinateRegion: $region, showsUserLocation: true)
+                .onAppear(perform: {
+                    locationManager.requestLocation()
+                    if let location = locationManager.lastKnownLocation {
+                        region.center = location.coordinate
+                        reverseGeocode(location: location)
+                    }
+                })
+                .frame(width: 250, height: 250)
+            
+            Button(action: {
+                if let address = address {
+                    // Aquí puedes usar la dirección para compartir
+                    print("Dirección: \(address)")
                 }
-            })
-            .frame(height: 300) // Ajusta el tamaño según tus necesidades.
+            }) {
+                Text("Share")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+        }
+    }
+    
+    private func reverseGeocode(location: CLLocation) {
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if let placemark = placemarks?.first {
+                if let name = placemark.name,
+                   let locality = placemark.locality,
+                   let administrativeArea = placemark.administrativeArea,
+                   let postalCode = placemark.postalCode,
+                   let country = placemark.country {
+                    self.address = "\(name), \(locality), \(administrativeArea) \(postalCode), \(country)"
+                } else {
+                    self.address = "Dirección no disponible"
+                }
+            } else {
+                self.address = "Dirección no disponible"
+            }
+        }
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+
+
+
+struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        ContentView()
     }
 }

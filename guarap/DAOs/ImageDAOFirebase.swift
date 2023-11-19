e443e5e91c638347496847ba17e742f2d2208516
est-gonzalezr <74991415+est-gonzalezr@users.noreply.github.com>
2023-11-18 22:39:50 -0500

    Fix getImageFromUrl

--- guarap/DAOs/ImageDAOFirebase.swift
+++ guarap/DAOs/ImageDAOFirebase.swift
@@ -16,27 +16,55 @@ class ImageDAOFirebase: ImageDAO {
     static var shared: ImageDAO = ImageDAOFirebase()
     
     func getImageFromUrl(url: String, completion: @escaping (UIImage?) -> Void) {
-        let storage = Storage.storage()
-        let storageRef = storage.reference(forURL: url)
-        
-        storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
-            if let error = error {
-                print("Error downloading image: \(error)")
-                completion(nil)
-            } else {
-                // Successfully downloaded image data
-                if let imageData = data {
-                    if let image = UIImage(data: imageData) {
-                        completion(image)
+        if url == "" {
+            completion(nil)
+        }
+        else if let storageUrl = URL(string: url), storageUrl.scheme == "gs" {
+            // Firebase Storage URL
+            let storage = Storage.storage()
+            let storageRef = storage.reference(forURL: url)
+            
+            storageRef.getData(maxSize: 1 * 1024 * 1024) { data, error in
+                if let error = error {
+                    print("Error downloading image: \(error)")
+                    completion(nil)
+                } else {
+                    if let imageData = data {
+                        if let image = UIImage(data: imageData) {
+                            completion(image)
+                        } else {
+                            print("Error creating UIImage from data")
+                            completion(nil)
+                        }
                     } else {
-                        print("Error creating UIImage from data")
+                        print("No image data found")
                         completion(nil)
                     }
-                } else {
-                    print("No image data found")
-                    completion(nil)
                 }
             }
+        } else if let imageUrl = URL(string: url), imageUrl.scheme == "http" || imageUrl.scheme == "https" {
+            // Regular HTTP/HTTPS URL
+            URLSession.shared.dataTask(with: imageUrl) { data, response, error in
+                if let error = error {
+                    print("Error downloading image: \(error)")
+                    completion(nil)
+                } else {
+                    if let imageData = data {
+                        if let image = UIImage(data: imageData) {
+                            completion(image)
+                        } else {
+                            print("Error creating UIImage from data")
+                            completion(nil)
+                        }
+                    } else {
+                        print("No image data found")
+                        completion(nil)
+                    }
+                }
+            }.resume()
+        } else {
+            print("Invalid URL format")
+            completion(nil)
         }
     }
     
